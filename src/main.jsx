bf2e5aed393c96aa17c3ea1badbfbd4886a9fd9c
Junhyun Kim <linokim220@gmail.com>
2024-08-24 03:50:21 +0900

    [style] remove default styles, add global styledcomponent styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyle from "./styles/GloabalStyles.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <>
+    <GlobalStyle />
     <App />
-  </StrictMode>,
-)
+  </>
+);
